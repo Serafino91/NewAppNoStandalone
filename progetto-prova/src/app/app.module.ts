bbed7017f738f8017087b4045ca4a93963e32e9f
sasa10 <s.conforti29@gmail.com>
2025-02-15 09:16:25 +0100

    aggiunti nuovi componenti fatti dal corso

--- progetto-prova/src/app/app.module.ts
+++ progetto-prova/src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../components/header/header.component';
-import { ButtonScComponent } from "../components/button-sc/button-sc.component";
+import { ButtonScComponent } from '../components/button-sc/button-sc.component';
+import { RecipesComponent } from '../components/recipes/recipes.component';
+import { ShoppingListComponent } from '../components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from '../components/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from '../components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from '../components/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from '../components/recipes/recipe-detail/recipe-detail.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonScComponent,
-    ButtonScComponent
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
